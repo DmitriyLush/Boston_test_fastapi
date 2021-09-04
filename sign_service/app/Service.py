9c7cb6e8d725a6e52fff719feca4dbb4e474d20c
DmitriyLush <aorti0717@gmail.com>
2021-09-04 05:27:55 +0300

    Update

--- sign_service/app/Service.py
+++ sign_service/app/Service.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import pandas as pd
 
 app = FastAPI()
@@ -9,16 +9,24 @@ patients = pats.set_index('patient_identifier')
 
 
 @app.get('/', status_code=200)
-def get_all_signatures_for_all():
-    return [(i, dict(patients.loc[i])) for i in patients.index]
+def get_all_signatures_for_all() -> dict:
+    all_patients_info = {}
+    for i in patients.index:
+        all_patients_info[i] = dict(patients.loc[i])
+    return all_patients_info
 
 
 @app.get('/{name}', status_code=200)
-def get_all_signatures_for_one(name: str):
-    return {name: patients.loc[name]}
+def get_all_signatures_for_one(name: str) -> dict:
+    if name not in patients.index.tolist():
+        raise HTTPException(status_code=404, detail='Name not found')
+    return {'Patient': name,
+            'Patient signatures': patients.loc[name]}
 
 
 @app.get('/{name}/{signature_name}', status_code=200)
-def get_signature(name: str, signature_name: str):
-    return {
-        f"Для пациента {name} показателем {signature_name} является: {dict(patients.loc[name])[signature_name]}"}
+def get_signature(name: str, signature_name: str) -> dict:
+    if name not in patients.index.tolist() or signature_name not in dict(patients.loc[name]):
+        raise HTTPException(status_code=404, detail='Name or signature not found')
+    return {'Patient': name,
+            'Patient signature': dict(patients.loc[name])[signature_name]}
